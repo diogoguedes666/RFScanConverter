27a37f5b8032b0b3020086f0ea274774eafc38cf
diogoguedes666 <110556059+diogoguedes666@users.noreply.github.com>
2024-05-09 10:45:53 -0300

    Delete import tkinter as tk.py

--- import tkinter as tk.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-
-root = tk.Tk()
-root.title("Test Tkinter Installation")
-label = tk.Label(root, text="If this window displays, Tkinter is installed correctly!")
-label.pack()
-root.mainloop()
